319d8f826a261e304c4e192763d02ba274421f9b
Claudia Ligidakis <claudia.ligidakis@avanade.com>
2019-06-28 10:56:11 -0700

    added indexing for the grant columns

--- src/components/BottomFilter.vue
+++ src/components/BottomFilter.vue
@@ -6,6 +6,11 @@
                     <button v-on:click="resetGrants" class="text-uppercase btn-style"><</button>
                 </b-button-group>
             </b-col>
+            <b-col cols="2">
+                <div>
+                   SELECT YEAR OPTIONS
+                </div>
+            </b-col>
             <b-col cols="1" offset="5">
                 <i class="material-icons md-48">search</i>
             </b-col>
@@ -15,19 +20,30 @@
 
 <script>
     import search from '@/components/Search.vue';
+    import { mapActions, mapState } from 'vuex';
 
     export default {
         name: 'bottomFilter',
         components: {
             search
         },
+        mounted() {
+            this.$store.commit('SET_YEARS')
+        },
         methods: {
             resetGrants() {
                 this.$store.dispatch('resetState')
-                    .then((res) => {
+                    .then(() => {
                         this.$router.push('/')
                     })
+            },
+            updateYearFilter(selected) {
+                this.$store.commit('FILTER_YEAR', selected)
             }
+        },
+        computed: {
+            ...mapActions(['resetState']),
+            ...mapState(['years', 'year'])
         }
 }
 </script>
