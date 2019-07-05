c7991b8ff2781d04ae939a0216d9b29aa5cb1394
Jordan Smith <jordan.m.smith721@gmail.com>
2019-07-05 14:40:16 -0700

    Implemented countUp on headers

--- Client/src/components/ListTotal.vue
+++ Client/src/components/ListTotal.vue
@@ -2,10 +2,12 @@
     <b-container class="grant-total">
         <b-row align-h="center">
             <b-col cols="3">
-                <span class="counter">{{ numberWithCommas(grantCount) }}</span><span class="grant-text">GRANTS</span>
+                <ICountUp :endVal="grantCount" :options="options" class="counter"></ICountUp><span class="grant-text">GRANTS</span>
+                <!-- <span class="counter">{{ numberWithCommas(grantCount) }}</span><span class="grant-text">GRANTS</span> -->
             </b-col>
             <b-col cols="6">
-                <span class="counter">${{ numberWithCommas(grantTotal) }}</span> <span class="grant-text">{{$route.name == 'topic-grants' ? null : (topic.name ? 'for ' + topic.name : null)}}  {{$route.name == 'region-grants' ? null : (region.name ? 'in ' + region.name : null)}} {{year.name ? 'during ' + year.name : 'provided since 1999'}}</span>
+                <ICountUp :endVal="grantTotal" :options="amountOptions"  class="counter"></ICountUp> <span class="grant-text">{{$route.name == 'topic-grants' ? null : (topic.name ? 'for ' + topic.name : null)}}  {{$route.name == 'region-grants' ? null : (region.name ? 'in ' + region.name : null)}} {{year.name ? 'during ' + year.name : 'provided since 1999'}}</span>
+                <!-- <span class="counter">${{ numberWithCommas(grantTotal) }}</span> <span class="grant-text">{{$route.name == 'topic-grants' ? null : (topic.name ? 'for ' + topic.name : null)}}  {{$route.name == 'region-grants' ? null : (region.name ? 'in ' + region.name : null)}} {{year.name ? 'during ' + year.name : 'provided since 1999'}}</span> -->
             </b-col>
         </b-row>
         <div class="filter">{{$route.name == 'search-grants' ? 'Search Results:' : null}} {{$route.params.filter ? transform($route.params.filter) : null}}</div>
@@ -14,8 +16,23 @@
 
 <script>
     import {mapState} from 'vuex'
+    import ICountUp from 'vue-countup-v2';
     export default {
         name: 'listTotal',
+        components: {
+            ICountUp
+        },
+        data() {
+            return {
+                options: {
+                    separator: ','
+                },
+                amountOptions: {
+                    separator: ',',
+                    prefix: '$'
+                }
+            }
+        },
         methods: {
             transform(value) {
                 return value.replace("-", " ")
@@ -38,8 +55,6 @@
 
     .counter {
         display: inline-block;
-        -webkit-animation: flip .2s 2 linear;
-        animation: flipX .2s 2 linear;
     }
 
     .filter {
